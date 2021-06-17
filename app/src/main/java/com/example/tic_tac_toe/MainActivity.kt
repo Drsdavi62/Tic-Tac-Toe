b37a1e3af28372334c0db08c38db4be7925ecbc9
Davi Ribeiro dos Santos <davi.ribeirosantos11@gmail.com>
2021-06-17 15:06:11 -0300

    Splash screen

--- app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
@@ -29,6 +29,7 @@ class MainActivity : ComponentActivity() {
     @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_TicTacToe_NoActionBar)
         
         setContent {
             TicTacToeTheme {
